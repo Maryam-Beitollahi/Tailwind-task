b246c73887ed56b8a4ba7cdc32df09e6181e4cb1
maryam <maryambeitollahi71@gmail.com>
2024-10-10 12:37:52 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Vazirmatn"],
+      },
+      colors:{
+        "secondary800" : "#3F4264",
+        "secondary900" : "#2A2D53",
+        "secondary600" : "#6A6C87",
+        "Primary900" : "#4A6DFF",
+        "secondary200" : "#BFC0CB",
+        "secondary700" : "#555775",
+        "other" : "#FF9900",
+        "newColor" : "#36385380",
+        "red" : "#FF0000CC",
+        "button" :"#4A6DFF",
+      },
+      fontSize:{
+        "new" : "8px",
+        "verysmall" : "6px",
+        "md" : "16px",
+        "buttonText" : "19.66px",
+        "verybig" : "40px",
+      },
+      fontWeight:{
+        "verybold" : "900px",
+        "veryverybold" : "1000px",
+      },
+      borderRadius:{
+       "newradius": "426.8px",
+       "twenty" : "20px",
+      },
+    },
+  },
+  plugins: [],
+};
